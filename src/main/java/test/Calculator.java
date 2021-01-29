379df33ba0692b4318ccc859534cf1fbe1cf5908
eomjinyoung <jinyoung.eom@gmail.com>
2021-01-29 16:44:54 +0900

    .

--- src/main/java/test/Calculator.java
+++ src/main/java/test/Calculator.java
@@ -1,9 +1,14 @@
 package test;
 
 public class Calculator {
-  int result = 327;
+  int result;
 
   void plus(int value) {
     this.result += value;
   }
+
+  static int plus2(int a, int b) {
+    return a + b;
+  }
+
 }
